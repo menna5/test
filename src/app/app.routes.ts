d72e6d15198cc184fe61700fe012b33943182a2c
Menna <Mena08800@gmail.com>
2025-07-13 19:46:23 +0300

    initiatlizing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/Components/not-found/not-found.component';
+import { LoginComponent } from './auth/Components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent },
+    {path: '**', component: NotFoundComponent}
+];
---
+++ src/app/auth/Components/login/login.component.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  http = inject(HttpClient);
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.http.post('https://dummyjson.com/auth/login', this.loginForm.value).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+          // Handle successful login
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+          // Handle login error
+        }
+      });
+    }
+  }
+
+  onGoogleLogin(): void {
+    console.log('Google login clicked');
+    // Add your Google Auth logic here
+  }
+
+  onForgotPassword(): void {
+    console.log('Forgot password clicked');
+    // Navigate to Forgot Password page or modal
+  }
+}
